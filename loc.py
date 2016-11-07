6dc6a97458f17490d6a1cfcfe5cada31f2a86d76
forgoodsj <877848146@qq.com>
2016-11-07 17:38:28 +0800

    改bug

--- loc.py
+++ loc.py
@@ -6,11 +6,13 @@ global s
 s = requests.session()
 
 def loc(openid):
-    url = 'https://api.weixin.qq.com/cgi-bin/user/info/batchget?access_token=t6FgbziDST6e5mY3uM6ZBhBIQo8LhDUPROSsBacW17T'
+    url = 'https://api.weixin.qq.com/cgi-bin/user/info/batchget?access_token=weixin'
     da = {"openid": openid, "lang": "zh-CN"}
     data = json.dumps(da)
     r = s.post(url, data=data)
     j = eval(r.text)
     city = j['user_info_list'][0]['city']
     return city
+
+
     
