9d05a0af86788cd3b59855b1dea8fa5515f55b43
forgoodsj <877848146@qq.com>
2016-11-10 16:16:00 +0800

    新增菜谱图片

--- talk_api.py
+++ talk_api.py
@@ -42,8 +42,18 @@ def talk(content, userid):
         #recontent = (code,j['text'],j['list'][0]['name'],j['list'][0]['info'],j['list'][0]['icon'],j['list'][0]['detailurl'])
         recontent = []
         num = 0
+        picurl=['https://mmbiz.qlogo.cn/mmbiz_jpg/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GibSfY3O03ZdTtZrcCRCNFcl3eNOZ5PiabmC5fMJHECpw2IJvJzjrVZrA/0?wx_fmt=jpeg',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GTRC5lsfAtOO0OoWtxVRhyibeSKnPNHcdng82iaa5RO7y8iaIaf6k7j6aA/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GTRC5lsfAtOO0OoWtxVRhyibeSKnPNHcdng82iaa5RO7y8iaIaf6k7j6aA/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GyZzgfvDZc2Pg4xEpJicT3Wt11XzveblXfOCQLASJ1IaphRzFSaHrpicg/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GlXlL8MASn2ibR2CJTCZBNpQXx17iaoIP3W3Gaha4uCTydZTQkGxRhiciaQ/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GmPPG7CbVxGOCicXibDzbBums59sfN9LbmTwicHZl2muzuvqcicmAR2BILQ/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GYA6IvaAN7zxTDRp2oeI96FN7bShg009ibf6mcwzXGb9QS6wFUwOGr8g/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GTzDL1Wjvg6jo8TgP9jEZibvOlJXBmgic7aaXnLvUTRibssS46nlhzQ28g/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2Gibatuvu8ibL0AiaEwLlsCE9QaiayuGZELHfRa9Pk1P2axbXNDRd41ft5lg/0?wx_fmt=png',
+        'https://mmbiz.qlogo.cn/mmbiz_png/w7XYZOGUbVEE0zl6MWr1MZj695AtnK2GLklDoiao3dpOalSUSRbolVcPl3svoF04Av7aZuZic9rCKIdGZmClct0A/0?wx_fmt=png']        
         for n in j['list']:
-            recontent.append((n['name'],n['info'],n['icon'],n['detailurl']))
+            recontent.append((n['name'],n['info'],picurl[num],n['detailurl']))
             if num < 10:
                 num = num + 1
             else:
