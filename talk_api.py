fb9116cbe773ebd58915c228e5ed2bcd50bdaf54
forgoodsj <877848146@qq.com>
2016-11-10 15:05:01 +0800

    不是吧

--- talk_api.py
+++ talk_api.py
@@ -34,7 +34,7 @@ def talk(content, userid):
         for n in j['list']:
             recontent.append((n['article'],n['source'],n['icon'],n['detailurl']))
             num = num + 1
-            if num > 2:
+            if num > 1:
                 break
             return (code , recontent, num)
     elif code == 308000:
