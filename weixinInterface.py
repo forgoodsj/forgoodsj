f94922bd87be6a03cfa81b6b3917a5ae7b19886e
forgoodsj <877848146@qq.com>
2016-11-27 11:08:07 +0800

    大跃进

--- weixinInterface.py
+++ weixinInterface.py
@@ -56,76 +56,80 @@ class WeixinInterface:
             content = xml.find("Content").text
         elif msgType == 'voice':
             content = xml.find("Recognition").text
-            if content == u"今天是什么日子":
-                d1 = datetime.datetime.now()
-                d2 = datetime.datetime(2013,11,4)
-                timedelta = str(d1 - d2)[:4]
-                if timeNow[8:10] =="04":
-                    if timeNow[5:10] == "11-04":
-                        year = int(timeNow[0:4]) - 2013
-                        reply = u'哇！今天是叔叔和baby的%s周年纪念日~好棒呀！我们要白头偕老~'%year
-                        return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
-                    else:
-                        reply = u'哇！今天是叔叔和baby的纪念日,在一起%s天啦~一定要好好庆祝呀~'%timedelta
-                        return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
-                elif timeNow[5:10]=="05-28":
-                    reply = u'baby~生日快乐~呆呆和叔叔永远爱你~baby和叔叔在一起%s天啦~'%timedelta
-                    return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
-                else:
-                    #key = 'd2ddb6c8b6d84b4c8c278868ec74fcae'
-                    #api = 'http://www.tuling123.com/openapi/api?key=' + key + '&info='
-                    #info = content.encode('utf-8')
-                    #url = api + info
-                    #page = urllib.urlopen(url)
-                    #html = page.read()
-                    #dic_json = json.loads(html)
-                    #reply_content = dic_json['text']                    
-                    info = content.encode('utf-8')
-                    msg = talk_api.talk(info,userid)
-                    timereply = u'   哇赛！今天是叔叔和baby在一起的第%s天啦~~'%timedelta
-                    timereply1 =timereply.encode('utf-8')
-                    reply = msg + timereply1
-                    return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
-                    
-            elif content == u"我的id":
-                return self.render.reply_text(fromUser,toUser,int(time.time()),fromUser)
-            
-            elif content[0:3] == u"百度云":
-                info = content[3:]
-                msg = pan.pan(info)
-                return self.render.reply_news(fromUser,toUser,int(time.time()),msg[0],msg[1])
-                #return self.render.reply_text(fromUser,toUser,int(time.time()),msg)
-                
-            
-            elif u'电影' in content:
-                msg = movietop10.parse_html()
-                return self.render.reply_news(fromUser,toUser,int(time.time()),msg[0],msg[1])
-            
-            else: 
-                info = content.encode('utf-8')
-                msg = talk_api.talk(info,userid)
-                if isinstance(msg,str):
-                    return self.render.reply_text(fromUser,toUser,int(time.time()),msg)
-                elif isinstance(msg,tuple):                   
-                    if msg[0] == 200000:
-                        #return self.render.reply_text(fromUser,toUser,int(time.time()),info)
-                        #return self.render.reply_text(fromUser,toUser,int(time.time()),msg[1])
-                        #return self.render.reply_text(fromUser,toUser,int(time.time()),msg[2])
-                        return self.render.reply_onenew(fromUser,toUser,int(time.time()),content,msg[1],'',msg[2])
-                    elif msg[0] == 302000:                        
-                        return self.render.reply_news(fromUser,toUser,int(time.time()),msg[1],msg[2])                        
-                    elif msg[0] == 308000:
-                        return self.render.reply_news(fromUser,toUser,int(time.time()),msg[1],msg[2])   
-                                        
-                else:
-                    return self.render.reply_text(fromUser,toUser,int(time.time()),u'呆呆出问题了')
-        
         elif msgType == 'image':
             try:
                 media_id = 'adH7gfYGXJogSSPBi5mNNzl2pf8czDj_PekDVY5aSoncczqDtVmGR9j2F5nbi_VS'
                 return self.render.reply_image(fromUser, toUser, int(time.time()), media_id)
             except:
                 return self.render.reply_text(fromUser, toUser, int(time.time()),  '呆呆看不懂图片哎')
+        
+        return content
+    
+    def reply(self):
+        content = POST(self)
+        if content == u"今天是什么日子":
+            d1 = datetime.datetime.now()
+            d2 = datetime.datetime(2013,11,4)
+            timedelta = str(d1 - d2)[:4]
+            if timeNow[8:10] =="04":
+                if timeNow[5:10] == "11-04":
+                    year = int(timeNow[0:4]) - 2013
+                    reply = u'哇！今天是叔叔和baby的%s周年纪念日~好棒呀！我们要白头偕老~'%year
+                    return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
+                else:
+                    reply = u'哇！今天是叔叔和baby的纪念日,在一起%s天啦~一定要好好庆祝呀~'%timedelta
+                    return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
+            elif timeNow[5:10]=="05-28":
+                reply = u'baby~生日快乐~呆呆和叔叔永远爱你~baby和叔叔在一起%s天啦~'%timedelta
+                return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
+            else:
+                #key = 'd2ddb6c8b6d84b4c8c278868ec74fcae'
+                #api = 'http://www.tuling123.com/openapi/api?key=' + key + '&info='
+                #info = content.encode('utf-8')
+                #url = api + info
+                #page = urllib.urlopen(url)
+                #html = page.read()
+                #dic_json = json.loads(html)
+                #reply_content = dic_json['text']                    
+                info = content.encode('utf-8')
+                msg = talk_api.talk(info,userid)
+                timereply = u'   哇赛！今天是叔叔和baby在一起的第%s天啦~~'%timedelta
+                timereply1 =timereply.encode('utf-8')
+                reply = msg + timereply1
+                return self.render.reply_text(fromUser,toUser,int(time.time()),reply)
+                    
+        elif content == u"我的id":
+            return self.render.reply_text(fromUser,toUser,int(time.time()),fromUser)
+            
+        elif content[0:3] == u"百度云":
+            info = content[3:]
+            msg = pan.pan(info)
+            return self.render.reply_news(fromUser,toUser,int(time.time()),msg[0],msg[1])
+            
+                
+            
+        elif u'电影' in content:
+            msg = movietop10.parse_html()
+            return self.render.reply_news(fromUser,toUser,int(time.time()),msg[0],msg[1])
+            
+        else: 
+            info = content.encode('utf-8')
+            msg = talk_api.talk(info,userid)
+            if isinstance(msg,str):
+                return self.render.reply_text(fromUser,toUser,int(time.time()),msg)
+            elif isinstance(msg,tuple):                   
+                if msg[0] == 200000:
+                    
+                    return self.render.reply_onenew(fromUser,toUser,int(time.time()),content,msg[1],'',msg[2])
+                elif msg[0] == 302000:                        
+                    return self.render.reply_news(fromUser,toUser,int(time.time()),msg[1],msg[2])                        
+                elif msg[0] == 308000:
+                    return self.render.reply_news(fromUser,toUser,int(time.time()),msg[1],msg[2])   
+                                        
+            else:
+                return self.render.reply_text(fromUser,toUser,int(time.time()),u'呆呆出问题了')
+        
+        
                    
         '''              
             
